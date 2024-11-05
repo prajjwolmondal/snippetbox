2e9ff181c393133263f1cf355c602d1a510b4686
Prajjwol Mondal <prajj.mondal@gmail.com>
2024-11-05 17:14:58 -0500

    Completed chapter 9

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"log/slog"
 	"net/http"
@@ -65,6 +66,9 @@ func main() {
 	sessionManager.Store = pgxstore.New(dbpool)
 	sessionManager.Lifetime = 12 * time.Hour
 
+	// This means that the cookie will only be sent by a user's web browser when a HTTPS connection is being used
+	sessionManager.Cookie.Secure = true
+
 	app := &application{
 		logger:         logger,
 		snippets:       &models.SnippetModel{DbPool: dbpool},
@@ -73,11 +77,28 @@ func main() {
 		sessionManager: sessionManager,
 	}
 
+	// Hold the non-default TLS settings we want the server to use. In this case the only
+	// thing that we're changing is the curve preferences value, so that only elliptic curves
+	// with assembly implementations are used.
+	tlsConfig := &tls.Config{
+		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
+	}
+
+	server := &http.Server{
+		Addr:         *addr,
+		Handler:      app.routes(),
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelWarn), // Logging server errors at Warn level
+		TLSConfig:    tlsConfig,
+		IdleTimeout:  time.Minute,      // all keep-alive connections will be automatically closed after 1 min of inactivity
+		ReadTimeout:  5 * time.Second,  // if the request headers or body are still being read 5 seconds after the request is first accepted, then Go will close the underlying connection. This helps to mitigate the risk from slow-client attacks such as Slowloris
+		WriteTimeout: 10 * time.Second, // will close the underlying connection if our server attempts to write to the connection after the given period.  If using HTTPS it’s sensible to set WriteTimeout to a value greater than ReadTimeout.
+	}
+
 	// The value returned from the flag.String() function is a pointer to the flag
 	// value, not the value itself.
-	logger.Info("starting server", slog.String("addr", ":4000"))
+	logger.Info("starting server", slog.String("addr", server.Addr))
 
-	err = http.ListenAndServe(*addr, app.routes())
+	err = server.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
